e2a6e7be51870c4bfa99e338f375c4958cde72ae
Robert Arthur <robertarthur318@gmail.com>
2024-10-02 06:58:32 +0000

    Format files and add CORS

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from fastapi import FastAPI
 
 from db import database
 from resources.routes import api_router
+from starlette.middleware import CORSMiddleware
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:4200"
+]
 
 app = FastAPI()
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
